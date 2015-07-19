6bcdf3502b9761f6d81982ea29e7828cb1c4f393
morganecf <morganeciot@gmail.com>
2015-07-19 19:10:30 -0400

    starting playing iwth add note functionality

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
+/* Stores notes */
+var notes = {};
+
 /* Function to populate the pop up with notes */
 function set_notes (notes) {
 	for (var i = 0; i < notes.notes.length; i++) {
@@ -10,9 +13,26 @@ function set_notes (notes) {
 	send request for notes if the note thing is clicked on 
 */
 window.addEventListener('DOMContentLoaded', function () {
+	// Add new note button click functionality 
+	$(".btn-info").click(function () {
+		console.log("clicked on new note");
+		$("#note-name-div").show();
+	});
+	$(".btn-success").click(function () {
+		// Get what's in the note name field
+		var note_name = $("#note-name").val();
+		notes.note_name = [];
+
+		$("#note-section").append($("<h4>" + note_name + "</h4>"));
+
+		$("#note-name-div").hide();
+
+		// TO DO : Error handling (ex: no note name), Save to file system 
+	});
+
+	// Query for notes 
 	var query_info = {active: true, currentWindow: true};
 	chrome.tabs.query(query_info, function (tabs) {
-		console.log("sending request for notes");
 		// Send request for notes with callback to populate 
 		chrome.tabs.sendMessage(tabs[0].id, {from: 'popup', subject: 'view-notes'}, set_notes);
 	});
