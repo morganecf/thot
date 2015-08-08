e9d0d4eb15dde1cb183f8dfc210dbf95cd92ec2e
morganecf <morganeciot@gmail.com>
2015-08-08 17:03:15 -0400

    highlight functionality

--- notes.js
+++ notes.js
@@ -1,14 +1,23 @@
 /* Saves all notes */
 var notes = [];
 
+/* Function that (literally) highlights a note */
+var highlight = function (range) {
+  var new_node = document.createElement("div");
+  new_node.setAttribute("style", "background-color:yellow; display:inline");
+  range.surroundContents(new_node);
+};
 
 /* Function that grabs a note */
 var note = function () {
   var text = '';
+  var selection = window.getSelection();
   // If something has been highlighted 
-  if (window.getSelection()) {
+  if (selection) {
     // Get the highlighted text 
-    text = window.getSelection().toString();
+    text = selection.toString();
+    // Get where the highlighted text starts 
+    highlight(selection.getRangeAt(0));
   }
   else if (document.selection && document.selection.type != "Control") {
         text = document.selection.createRange().text;
@@ -20,21 +29,19 @@ var note = function () {
 $(document).mouseup(function () {
 	var text = note();
 	notes.push(text);
-
-	console.log("highlighted:", text);
-
-	// Send to the background script
-	// chrome.runtime.sendMessage({note: text}, function (response) {
-	// 	console.log(response.received);
-	// });
-
 });
 
 /* Listen for a message from the popup - when it pops up, display notes */
 chrome.runtime.onMessage.addListener(function (msg, sender, response) {
 	if (msg.from === 'popup' && msg.subject === 'view-notes') {
-		console.log("message received from popup");
-		// respond to popup with the notes 
+		// Respond to popup with the notes 
 		response({notes: notes});
 	}
 });
+
+// TO DO: actually highlight what is highlighted 
+// color themes 
+// save to file system 
+// new note 
+// history of notes 
+// edit capability
